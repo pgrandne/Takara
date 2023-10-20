f0efedeeda15a62718c213c2e72bd497945179c6
perring <perrin.grandne@gmail.com>
2023-10-20 17:15:02 +0200

    Add gradient

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,11 +7,13 @@ import { WagmiConfig } from 'wagmi'
 import { config } from '../wagmi'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = React.useState(false)
-  React.useEffect(() => setMounted(true), [])
-  return (
-    <WagmiConfig config={config}>
-      <ConnectKitProvider>{mounted && children}</ConnectKitProvider>
-    </WagmiConfig>
-  )
+	const [mounted, setMounted] = React.useState(false)
+	React.useEffect(() => setMounted(true), [])
+	return (
+		<WagmiConfig config={config}>
+			<ConnectKitProvider theme='retro'>
+				{mounted && children}
+			</ConnectKitProvider>
+		</WagmiConfig>
+	)
 }
