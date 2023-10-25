4eeaa07b12213af4bafa879d719db9988fab31a4
perring <perrin.grandne@gmail.com>
2023-10-25 12:06:28 +0200

    correction if already played

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -6,21 +6,33 @@ import {
 	useContractWrite,
 	useWaitForTransaction,
 } from 'wagmi'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { toast } from 'react-toastify'
 import { takaraABI } from '../utils/takaraABI'
 import { takaraContract } from '../utils/contracts'
+
+interface IPlayer {
+	ticket: boolean
+	lastDayPlayed: number
+	lastPlot: number
+	winner: boolean
+}
 
 export const Game = () => {
 	const { address } = useAccount()
 	const [selectedPlot, selectPlot] = useState(400)
 	const [displayedPlot, displayPlot] = useState(400)
-	const [isPlayer, setIsPlayer] = useState(false)
+	const [currentDay, setCurrentDay] = useState(0)
+	const [isPlayer, setIsPlayer] = useState<IPlayer>({
+		ticket: false,
+		lastDayPlayed: 0,
+		lastPlot: 444,
+		winner: false,
+	})
 	const notify1 = () =>
 		toast('🦄 You transasaction is sent, please wait the result!')
 	const notify2 = () => toast('🥕🥕 were on this plot, try again tomorrow!')
 
-	// Ticket
+	// Player Status
 
 	const { data: dataPlayer, refetch: refetchPlayer } = useContractRead({
 		address: takaraContract,
@@ -28,10 +40,29 @@ export const Game = () => {
 		functionName: 'isPlayer',
 		args: [address!],
 		watch: true,
-		onSuccess(data: boolean) {
-			setIsPlayer(data)
+		onSuccess(data: [boolean, number, number, boolean]) {
+			setIsPlayer({
+				ticket: data[0],
+				lastDayPlayed: data[1],
+				lastPlot: data[2],
+				winner: data[3],
+			})
 		},
 	})
+
+	// Current day of Game
+
+	const { data: dataDay, refetch: refetchDay } = useContractRead({
+		address: takaraContract,
+		abi: takaraABI,
+		functionName: 'getCurrentDay',
+		watch: true,
+		onSuccess(data: number) {
+			setCurrentDay(data)
+		},
+	})
+
+	//Play the game
 
 	const { config, error } = usePrepareContractWrite({
 		address: takaraContract,
@@ -67,19 +98,29 @@ export const Game = () => {
 					className='plot'
 					onClick={() => {
 						selectPlot(i)
-						isPlayer
+						isPlayer.ticket && isPlayer.lastDayPlayed !== currentDay
 							? write?.()
+							: isPlayer.ticket
+							? window.alert('You already played today')
 							: window.alert('You need a ticket, please go on Account')
 					}}
 				></span>
 			)
 		else
 			plots.push(
-				<span key={i} className='plant carrot'>
-					<div className='plant carrot'>
-						<div className='bg'></div>
-					</div>
-				</span>
+				isPlayer.winner ? (
+					<span key={i} className='plant radish'>
+						<div className='plant radish'>
+							<div className='bg'></div>
+						</div>
+					</span>
+				) : (
+					<span key={i} className='plant carrot'>
+						<div className='plant carrot'>
+							<div className='bg'></div>
+						</div>
+					</span>
+				)
 			)
 	}
 
